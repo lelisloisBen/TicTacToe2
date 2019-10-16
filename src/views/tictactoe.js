0fdaaccd8cbb510138132eb689bf8346175a3449
samir benzada <samirbenzada@gmail.com>
2019-10-16 14:07:58 -0400

    update

--- src/views/tictactoe.js
+++ src/views/tictactoe.js
@@ -2,17 +2,22 @@ import React, { useState } from 'react';
 
 const Tic = () => {
 
-    const [playXO, setPlayXO] = useState('X');
+    const [playXO, setPlayXO] = useState('O');
+    const [num, setNum] = useState(9);
 
     const clickMe = (e) => {
-        (playXO === "X") ? setPlayXO("O") : setPlayXO("X");
         let divID = e.target.id;
+        (playXO === "X") ? setPlayXO("O") : setPlayXO("X");
         document.getElementById(divID).innerHTML = playXO;
+        document.getElementById(divID).className = "square noPointer";
         console.log(divID);
     }
 
-    const num = 9;
-    const random = Math.random();
+    const squareSelect = (e) => {
+        let val = Number(e.target.value);
+        console.log(val);
+        setNum(val);
+    }
 
     let containerWith = (Math.ceil(Math.sqrt(num))) * 200;
     let widthFinal = containerWith + 'px';
@@ -21,7 +26,7 @@ const Tic = () => {
     for (let i=0; i<num; i++) {
         let ID = (i+1);
         divid.push(
-            React.createElement('div', {onClick: clickMe, className: "square", id: ID, key: (i*random) }, "")
+            React.createElement('div', {onClick: clickMe, className: "square", id: ID, key: i }, "")
         );
     }
 
@@ -30,12 +35,22 @@ const Tic = () => {
     return (
         <>
         <h1 className="text-center" >Tic Tac Toe</h1>
+        <div className="selectSquares text-center">
+                <select id="" onChange={squareSelect}>
+                    <option value="">Number of squares</option>
+                    <option value="9">3x3</option>
+                    <option value="16">4x4</option>
+                    <option value="25">5x5</option>
+                    <option value="36">6x6</option>
+                </select>
+        </div>
         <br/>
             <div className="containe" style={{width: widthFinal}}>
                 <div className="row">
                     {divid}
                 </div>
             </div>
+            
         </>
     );
 };
