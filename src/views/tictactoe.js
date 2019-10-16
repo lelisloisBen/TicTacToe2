bb3701a79f894eadfbfb0f393425f31b0884c7cd
samir benzada <samirbenzada@gmail.com>
2019-10-16 10:11:51 -0400

    update

--- src/views/tictactoe.js
+++ src/views/tictactoe.js
@@ -2,22 +2,36 @@ import React from 'react';
 
 const Tic = () => {
 
+    // const [playXO, setPlayXO] = useState('');
+
+    const clickMe = (e) => {
+        // (playXO === "X") ? setPlayXO("O") : setPlayXO("X");
+        let divID = e.target.id;
+        document.getElementById(divID).innerHTML = "X";
+
+    }
+
     const num = 9;
     const random = Math.random();
-    // const container = React.createElement('div', {className: "square col-4", id: "12", key: random }, "X");
+
+    let containerWith = (Math.ceil(Math.sqrt(num))) * 200;
+    let widthFinal = containerWith + 'px';
 
     let divid = [];
     for (let i=0; i<num; i++) {
+        let ID = (i+1);
         divid.push(
-            React.createElement('div', {className: "square col-4", id: "12", key: (i*random) }, "X")
+            React.createElement('div', {onClick: clickMe, className: "square", id: ID, key: (i*random) }, "")
         );
     }
+
+    
 
     return (
         <>
         <h1 className="text-center" >Tic Tac Toe</h1>
         <br/>
-            <div className="container">
+            <div className="containe" style={{width: widthFinal}}>
                 <div className="row">
                     {divid}
                 </div>
